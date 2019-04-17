ccb8a939ce30552f6c037d80b108f81a3bb23e71
Robocraze <48519472+Robocraze@users.noreply.github.com>
2019-04-17 15:29:44 +0530

    BLE Scanner files for server and client devices

---
+++ BLE_client.ino
@@ -0,0 +1,104 @@
+/*
+ * A BLE client example to look for a particular BLE server
+ */
+
+#include <BLEDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+#define buzzer 12
+
+int beaconInRange = -1;
+int scanTime = 5;     // In seconds
+
+BLEScan *pBLEScan;
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+  void onResult(BLEAdvertisedDevice advertisedDevice)
+  {
+    if (advertisedDevice.getName() == "Robocraze - Easter Hunt")
+    {
+      int ble_rssi = advertisedDevice.getRSSI();
+      Serial.println("RSSI: " + String(ble_rssi));
+
+      if (ble_rssi > -85 && ble_rssi < -60)
+      {
+        beaconInRange = 0;
+      }
+      else if (ble_rssi > -60 && ble_rssi < -40)
+      {
+        beaconInRange = 1;
+      }
+      else if (ble_rssi > -40)
+      {
+        beaconInRange = 2;
+      }
+    }
+  }
+};
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(10);
+  pBLEScan->setWindow(9); // less or equal setInterval value
+
+  pinMode(buzzer, OUTPUT); // set pin to output mode
+}
+
+void loop()
+{
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  
+  Serial.println("Flag: " + String(beaconInRange));
+  
+  switch (beaconInRange)
+  {
+  case 0:
+    Serial.println("Beacon in Range");
+
+    for (int i = 0; i < 5; i++)
+    {
+      digitalWrite(buzzer, HIGH);
+      delay(400);
+      digitalWrite(buzzer, LOW);
+      delay(400);
+    }
+    break;
+  case 1:
+    Serial.println("Beacon is CLOSE!");
+
+    for (int i = 0; i < 10; i++)
+    {
+      digitalWrite(buzzer, HIGH);
+      delay(200);
+      digitalWrite(buzzer, LOW);
+      delay(200);
+    }
+    break;
+  case 2:
+    Serial.println("Beacon is VERY CLOSE!!!");
+
+    for (int i = 0; i < 20; i++)
+    {
+      digitalWrite(buzzer, HIGH);
+      delay(100);
+      digitalWrite(buzzer, LOW);
+      delay(100);
+    }
+    break;
+  default:
+    Serial.println("Beacon not in Range");
+    break;
+  }
+
+  pBLEScan->clearResults(); // delete results fromBLEScan buffer to release memory
+  beaconInRange = -1;
+}
---
+++ BLE_server.ino
@@ -0,0 +1,62 @@
+/*  
+ * A BLE Server example 
+ *   
+ */
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <Wire.h>
+#include <SH1106Wire.h>
+
+/* Use the following link to install OLED library: 
+  https://github.com/vlast3k/Arduino-libraries/tree/master/SSD1306 */
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+SH1106Wire oled(0x3c, 21, 22);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Wire.begin(21, 22);
+
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Robocraze - Easter Hunt");
+
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setValue("Easter Hunt");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+
+  BLEDevice::startAdvertising();
+
+  Serial.println("Characteristic defined!");
+
+  // OLED Display Initialization
+  oled.init();
+  delay(2);
+  oled.flipScreenVertically();
+  oled.setFont(ArialMT_Plain_16);
+  oled.drawString(5, 25, "The CraZy Ones");
+  oled.display();
+}
+
+void loop()
+{
+  // Nothing to add here. We only need the ESP to advertise BLE.
+}
